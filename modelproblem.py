5302b0db441b45c86454982512328fe9cb4a01a7
LarkinIt <caroline.larkin@pitt.edu>
2024-06-21 09:35:44 -0400

    Added citations to pyPESTO source code

--- modelproblem.py
+++ modelproblem.py
@@ -3,7 +3,7 @@ import roadrunner
 
 class ModelProblem:
     r"""
-    The model problem class that contains the libroadrunner simulator object and PEtab information. 
+    The model problem class that contains the libroadrunner simulator object and PEtab information. It is directly based on pyPESTO roadrunner importer (https://github.com/ICB-DCM/pyPESTO/tree/main/pypesto/objective/roadrunner) 
     
     Parameters
     ----------
@@ -14,9 +14,15 @@ class ModelProblem:
                  model_name : str):
        config_file = f"{model_name}/{model_name}.yaml"
        # load the PEtab problem
-       self.problem = petab.Problem.from_yaml(config_file)
+       self.petab_problem = petab.Problem.from_yaml(config_file)
+       
+       # verify PEtab problem is properly formatted
+       if petab.lint_problem(self.petab_problem):
+           raise ValueError("Something is wrong with the PEtab model >.<")
        
        # load the libroadrunner model
        model_file = f"{model_name}/model_{self.problem.model.model_id}.xml"
        self.simulator = roadrunner.RoadRunner(model_file)
+       
+       
        
