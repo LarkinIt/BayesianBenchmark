3a8ce9c5c542b2a84cd5733828ef0f95a9277a45
LarkinIt <caroline.larkin@pitt.edu>
2024-09-05 13:48:25 -0400

    Updated default ODE solver tolerances

--- modelproblem.py
+++ modelproblem.py
@@ -5,43 +5,6 @@ from petab.v1.parameters import get_priors_from_df
 from scipy.stats import uniform, norm
 import pypesto.objective.roadrunner as pypesto_rr
 from pypesto.objective import AggregatedObjective
-
-# Taken from https://stackoverflow.com/questions/63644282/how-to-replace-every-function-call-with-the-wrapper-call-in-the-original-class
-class ObjectiveWrapper(AggregatedObjective):
-	def __new__(cls, AggregatedObjective):
-		instance = super().__new__(cls) 
-		instance.module = AggregatedObjective
-
-		for funcname in dir(AggregatedObjective):
-			print(funcname)
-			if funcname != "__call__":
-				continue
-			else:
-				func = getattr(AggregatedObjective, funcname)
-				wrapped = ObjectiveWrapper._wrap(func)
-				setattr(instance, funcname, wrapped)
-				continue
-		return instance
-
-
-	@staticmethod
-	def _wrap(func):
-		"""
-		Wraps *func* with additional code.
-		"""
-		# we define a wrapper function. This will execute all additional code
-		# before and after the "real" function.
-		def wrapped(*args, **kwargs):
-			print("before-call:", func, args, kwargs)
-			output = func(*args, **kwargs)
-			print("after-call:", func, args, kwargs, output)
-			return output
-		# Use "update_wrapper" to keep docstrings and other function metadata
-		# intact
-		update_wrapper(wrapped, func)
-		# We can now return the wrapped function
-		return wrapped
-
 class ModelProblem():
 	def __init__(self, model_string):
 		self.model_name = model_string
@@ -56,16 +19,11 @@ class ModelProblem():
 
 		# set tolerances for ode solver
 		solver_options = pypesto_rr.SolverOptions(
-			relative_tolerance = 5e-8,
-			absolute_tolerance = 1e-6
+			relative_tolerance = 1e-16,
+			absolute_tolerance = 1e-8
 			)
-		"""print(type(problem.objective).__name__)
-		objective = problem.objective
-		wrapped_objective = ObjectiveWrapper(objective)
-		problem.objective - wrapped_objective
 		problem.objective.solver_options = solver_options
-		exit()
-		"""
+		
 		self.problem = problem
 		self.petab_problem = petab_problem
 		prior_info = get_priors_from_df(petab_problem.parameter_df,
