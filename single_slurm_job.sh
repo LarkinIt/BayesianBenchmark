d7f884db3430d2c3eb3d8aea1bff133fcb83a1d3
LarkinIt <caroline.larkin@pitt.edu>
2024-10-02 16:26:54 -0400

    Added first draft of automatic slurm job submission scripts

---
+++ single_slurm_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p any_cpu
+#SBATCH --cpus-per-task=32
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=caroline.larkin@pitt.edu
+#SBATCH --mem=92G
+
+export PATH=/net/dali/home/mscbio/cil8/.local/bin:$PATH
+
+module unload anaconda
+module load anaconda/3-cluster
+
+eval "$(conda shell.bash hook)"
+conda activate bayes
+
+python run_model_calibration.py -m ${method} -p ${problem} -n ${n_ensemble} -s ${seed} -o ${output_dir} -c ${SLURM_CPUS_PER_TASK}
+
+# Leave this line to tell slurm that the script finished correctly
+exit 0
---
+++ submit_jobs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+seeds=({0..99..1})
+declare -a probs=("Michaelis_Menten")
+declare -a methods=("smc", "pmc")
+n_ensemble=1000
+##### Declare anything else needed to pass as an arg below
+
+for prob in "${probs[@]}"; do
+	for method in "${methods[@]}"; do
+		for seed in "${seeds[@]}"; do
+			echo "PE problem: $prob  Method: $method  RNG seed: $seed"
+			outdir="./results/$prob/$method/"
+			if [ ! -d "$outdir" ]; then
+				mkdir -p $outdir
+			fi
+
+			logdir="outdir/logs/"
+			if [ ! -d "$logdir" ]; then
+				mkdir -p $logdir
+			fi
+			
+			job_name="$method.s$seed"
+			out_file="$logdir${job_name}.out"
+			err_file="$logdir${job_name}.err"
+
+			command="sbatch --job-name=$job_name --output=$out_file --error=$err_file --export=method=$method,problem=$prob,seed=$seed,output_dir=$outdir,n_ensemble=$n_ensemble single_slurm_job.sh"
+			echo "$command"
+			#sbatch --job-name=$job_name --output=$out_file --error=$err_file --export=method=$method,problem=$prob,seed=$seed,output_dir=$outdir,n_ensemble=$n_ensemble single_slurm_job.sh
+			#export goal_rna_method=$method
+			#export rna_heur_weight=$weight
+			#export seed=$seed 
+			#export output_dir=$prefix
+			#bash single_slurm_job.sh
+
+		done 
+	done
+done
