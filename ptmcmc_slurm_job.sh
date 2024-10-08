846dfc229fea03858adb84b7c12cc0be87f3cf77
LarkinIt <caroline.larkin@pitt.edu>
2024-10-07 20:47:04 -0400

    Added separate SLURM scripts for running PT-MCMC

---
+++ ptmcmc_slurm_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p any_cpu
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=END
+#SBATCH --mail-user=caroline.larkin@pitt.edu
+
+export PATH=/net/dali/home/mscbio/cil8/.local/bin:$PATH
+
+module unload anaconda
+module load anaconda/3-cluster
+
+eval "$(conda shell.bash hook)"
+conda activate bayes
+
+python run_model_calibration.py -m ${method} -p ${problem} -n ${n_ensemble} -s ${seed} -o ${output_dir} -c ${SLURM_CPUS_PER_TASK} -i ${n_iter}
+
+# Leave this line to tell slurm that the script finished correctly
+exit 0
