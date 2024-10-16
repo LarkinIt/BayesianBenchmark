02893fc4435a0623720adeabbde74479333daa03
LarkinIt <caroline.larkin@pitt.edu>
2024-10-15 21:59:58 -0400

    Added individual script for pairwise KS test in cases where it is too computational intensive

---
+++ pairwise_ks.py
@@ -0,0 +1,53 @@
+import glob
+import pickle
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from modelproblem import ModelProblem
+from result_classes import Result,MethodResults
+from tqdm import tqdm
+
+prob_name = "mRNA_self_reg"
+methods = ["smc", "pmc", "ptmcmc"]
+
+mod_prob = ModelProblem(prob_name)
+mod_prob.initialize()
+
+grouped_results = [MethodResults(x) for x in methods]
+
+for method, group_obj in zip(methods, grouped_results):
+	result_dir = f"results/{prob_name}/{method}/"
+	fnames = glob.glob(result_dir + "*.pkl")
+	for fname in fnames:
+		#print(fname)
+		with open(fname, "rb") as f:
+			results = pickle.load(f)
+		result_obj = Result(results)
+		group_obj.add_result(result_obj)
+print(type(mod_prob.problem))			
+
+fixed_idxs = mod_prob.problem.x_fixed_indices
+par_names = mod_prob.problem.x_names
+x=np.array(par_names)
+mask=np.full(len(par_names),True,dtype=bool)
+mask[fixed_idxs]=False
+fit_par_names=x[mask]
+
+all_ks_stats=[]
+
+ks_df = pd.DataFrame(columns=["Param", "Method", "KS"])
+for i, name in tqdm(enumerate(fit_par_names)):
+    # assumes all runs have the same model parameters
+    for runs in tqdm(grouped_results):
+        ks_stats , pvals = runs.calc_pairwise_matrix(par_index=i)
+        all_ks_stats.append(ks_stats[np.triu_indices(ks_stats.shape[0], k = 1)])
+        for run_ks in tqdm(ks_stats):
+            for ks_stat in run_ks:
+                new_row = {"Param":name, "Method":runs.abbr, "KS":ks_stat}
+                ks_df.loc[len(ks_df)] = new_row
+
+ks_df = ks_df.drop(ks_df[ks_df["KS"] == 0].index)
+
+sns.violinplot(data=ks_df, x="Param", y="KS", hue="Method")
+plt.savefig("ks_test.png")
