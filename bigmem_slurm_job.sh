d5b58e583ae80e0d8f3c8ca75186c16d4df1fe71
LarkinIt <caroline.larkin@pitt.edu>
2024-10-29 15:26:56 -0400

    Added slurm scripts to run specific seeds on big mem partition

---
+++ bigmem_slurm_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p big_memory
+#SBATCH --cpus-per-task=16
+#SBATCH --mail-type=END
+#SBATCH --mail-user=caroline.larkin@pitt.edu
+
+export PATH=/net/dali/home/mscbio/cil8/.local/bin:$PATH
+
+module unload anaconda
+module load anaconda/3-cluster
+
+eval "$(conda shell.bash hook)"
+conda activate bayes
+
+python run_model_calibration.py -m ${method} -p ${problem} -n ${n_ensemble} -s ${seed} -o ${output_dir} -c ${SLURM_CPUS_PER_TASK}
+
+# Leave this line to tell slurm that the script finished correctly
+exit 0
---
+++ submit_bigmem_jobs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+seeds=(27 32 57)
+declare -a probs=("Calcium_Oscillate")
+declare -a methods=("pmc")
+n_ensemble=1000
+##### Declare anything else needed to pass as an arg below
+
+for prob in "${probs[@]}"; do
+	for method in "${methods[@]}"; do
+		for seed in "${seeds[@]}"; do
+			echo "PE problem: $prob  Method: $method  RNG seed: $seed"
+			outdir="./results/$prob/$method/"
+			if [ ! -d "$outdir" ]; then
+				mkdir -p $outdir
+			fi
+
+			logdir="$outdir/logs/"
+			if [ ! -d "$logdir" ]; then
+				mkdir -p $logdir
+			fi
+			
+			job_name="$method.s$seed"
+			out_file="$logdir${job_name}.out"
+			err_file="$logdir${job_name}.err"
+
+			command="sbatch --job-name=$job_name --output=$out_file --error=$err_file --export=method=$method,problem=$prob,seed=$seed,output_dir=$outdir,n_ensemble=$n_ensemble single_slurm_job.sh"
+			echo "$command"
+			#sbatch --job-name=$job_name --output=$out_file --error=$err_file --export=method=$method,problem=$prob,seed=$seed,output_dir=$outdir,n_ensemble=$n_ensemble single_slurm_job.sh
+			#bash single_slurm_job.sh
+
+		done 
+	done
+done
