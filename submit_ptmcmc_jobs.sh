0ec6a8dfdab253bc19562b047a867312df8a44b9
LarkinIt <caroline.larkin@pitt.edu>
2024-11-01 19:16:07 -0400

    Rerun MM on cluster

--- submit_ptmcmc_jobs.sh
+++ submit_ptmcmc_jobs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 seeds=({0..99..1})
-declare -a probs=("linear_mRNA_self_reg")
+declare -a probs=("Michaelis_Menten")
 declare -a methods=("ptmcmc")
 n_ensemble=1000
-n_iter=230000
+n_iter=279000
 ##### Declare anything else needed to pass as an arg below
 
 for prob in "${probs[@]}"; do
