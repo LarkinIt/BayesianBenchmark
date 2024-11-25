27f302f8c23d9da2d94645180b81a64158fa5a87
LarkinIt <caroline.larkin@pitt.edu>
2024-11-25 11:56:26 -0500

    SLURM script to run hopf

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 seeds=({0..99..1})
-declare -a probs=("EGFR")
+declare -a probs=("Hopf")
 declare -a methods=("smc" "pmc")
 n_ensemble=1000
 ##### Declare anything else needed to pass as an arg below
